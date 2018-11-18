37ef1a4172aff66d7fa0e41e56bea6a8102bd332
Mo <Mo@LAPTOP-U8DTMQ2U>
2018-11-18 11:42:43 -0700

    Add project files.

---
+++ WallFly/Controllers/FileManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace WallFly.Controllers
+{
+    public class FileManager
+    {
+        private static readonly string StorageName = "mogeneral";
+        private static readonly string StorageApiKey = "WfpQSA5fHzhezp0OCjjVnJjNytggrmaBv+BUjIX4+JH6c75kPflcQu0RTIMUTSPeBPmGEyubnaRPDHL8OOLoIw==";
+        private static readonly string VideoContainer = "video";
+        private static readonly string AudioContainer = "audio";
+
+        public static IEnumerable<IListBlobItem> GetVideoBlobs()
+        {
+            CloudStorageAccount storageAccount = new CloudStorageAccount(
+                new Microsoft.WindowsAzure.Storage.Auth.StorageCredentials(
+                    StorageName, StorageApiKey), true);
+
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer cloudBlobContainer = blobClient.GetContainerReference(VideoContainer);
+            Task<BlobResultSegment> blobs = cloudBlobContainer.ListBlobsSegmentedAsync(new BlobContinuationToken());
+            return blobs.Result.Results;
+        }
+
+        public static IEnumerable<IListBlobItem> GetAudioBlobs()
+        {
+            CloudStorageAccount storageAccount = new CloudStorageAccount(
+                new Microsoft.WindowsAzure.Storage.Auth.StorageCredentials(
+                    StorageName, StorageApiKey), true);
+
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer cloudBlobContainer = blobClient.GetContainerReference(AudioContainer);
+            Task<BlobResultSegment> blobs = cloudBlobContainer.ListBlobsSegmentedAsync(new BlobContinuationToken());
+            return blobs.Result.Results;
+        }
+    }
+}
---
+++ WallFly/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.UI.Pages.Account.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.WindowsAzure.Storage.Blob;
+using WallFly.Models;
+
+namespace WallFly.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static string PASSWORD = "password";
+        public static string ADMIN = "admin";
+        public static string USER = "user";
+
+        public IActionResult Index()
+        {
+            if (NotAdmin())
+            {
+                return View();
+            }
+            return RedirectToAction("Video");
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.SetString(USER, "");
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Index(string password)
+        {
+            if (password == PASSWORD)
+            {
+                HttpContext.Session.SetString(USER, ADMIN);
+            }
+
+            return RedirectToAction("Video");
+        }
+
+        public IActionResult Video()
+        {
+            if(NotAdmin())
+            {
+               return RedirectToAction("Index");
+            }
+            IEnumerable<IListBlobItem> videoBlobs = FileManager.GetVideoBlobs();
+            return View(videoBlobs);
+        }
+
+        public IActionResult Audio()
+        {
+            if (NotAdmin())
+            {
+                return RedirectToAction("Index");
+            }
+            IEnumerable<IListBlobItem> audioBlobs = FileManager.GetAudioBlobs();
+            return View(audioBlobs);
+        }
+  
+        private bool NotAdmin()
+        {
+            return HttpContext.Session.GetString(USER) != ADMIN;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
